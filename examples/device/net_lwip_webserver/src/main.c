fcae442e13f845f4b35b504ed0939b73864408a3
Eka Susilo <ekawahyu@yahoo.com>
2024-12-26 18:19:40 -0800

    [WIP] attempt to add cdc-acm

--- examples/device/net_lwip_webserver/src/main.c
+++ examples/device/net_lwip_webserver/src/main.c
@@ -224,6 +224,54 @@ void tud_network_init_cb(void)
   }
 }
 
+// echo to either Serial0 or Serial1
+// with Serial0 as all lower case, Serial1 as all upper case
+static void echo_serial_port(uint8_t itf, uint8_t buf[], uint32_t count)
+{
+  uint8_t const case_diff = 'a' - 'A';
+
+  for(uint32_t i=0; i<count; i++)
+  {
+    if (itf == 0)
+    {
+      // echo back 1st port as lower case
+      if (isupper(buf[i])) buf[i] += case_diff;
+    }
+    else
+    {
+      // echo back 2nd port as upper case
+      if (islower(buf[i])) buf[i] -= case_diff;
+    }
+
+    tud_cdc_n_write_char(itf, buf[i]);
+  }
+  tud_cdc_n_write_flush(itf);
+}
+
+static void cdc_task(void)
+{
+  uint8_t itf;
+
+  for (itf = 0; itf < CFG_TUD_CDC; itf++)
+  {
+    // connected() check for DTR bit
+    // Most but not all terminal client set this when making connection
+    // if ( tud_cdc_n_connected(itf) )
+    {
+      if ( tud_cdc_n_available(itf) )
+      {
+        uint8_t buf[64];
+
+        uint32_t count = tud_cdc_n_read(itf, buf, sizeof(buf));
+
+        // echo back to both serial ports
+        echo_serial_port(0, buf, count);
+        echo_serial_port(1, buf, count);
+      }
+    }
+  }
+}
+
 int main(void)
 {
   /* initialize TinyUSB */
@@ -242,6 +290,7 @@ int main(void)
   while (1)
   {
     tud_task();
+    cdc_task();
     service_traffic();
   }
 
--- examples/device/net_lwip_webserver/src/tusb_config.h
+++ examples/device/net_lwip_webserver/src/tusb_config.h
@@ -91,11 +91,18 @@
 #endif
 
 //------------- CLASS -------------//
-
+#define CFG_TUD_CDC           2
 // Network class has 2 drivers: ECM/RNDIS and NCM.
 // Only one of the drivers can be enabled
 #define CFG_TUD_ECM_RNDIS     1
 #define CFG_TUD_NCM           (1-CFG_TUD_ECM_RNDIS)
+
+// CDC FIFO size of TX and RX
+#define CFG_TUD_CDC_RX_BUFSIZE   (TUD_OPT_HIGH_SPEED ? 512 : 64)
+#define CFG_TUD_CDC_TX_BUFSIZE   (TUD_OPT_HIGH_SPEED ? 512 : 64)
+
+// CDC Endpoint transfer buffer size, more is faster
+#define CFG_TUD_CDC_EP_BUFSIZE   (TUD_OPT_HIGH_SPEED ? 512 : 64)
 
 #ifdef __cplusplus
  }
